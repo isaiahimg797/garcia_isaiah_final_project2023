d784efbd523c55f0a4a004dfcba50f1d642b97e2
Isaiah <isaiahmgarcia09@gmail.com>
2023-12-07 09:30:42 -0800

    added drop down menu for excercises

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 # Sources:
     # Chris Cozort
     # https://www.dvlv.co.uk/pages/the-tkinter-cookbook.html
+    # https://www.geeksforgeeks.org/python-gui-tkinter/
 
 # Goals:
     # Create an app to track workouts
@@ -15,13 +16,58 @@ import tkinter as tk
 win = tk.Tk()
 win.geometry("600x600")
 
-def log(): 
-    new= Toplevel(win)
-    new.geometry("600x600")
-    new.title("New Window")
+# second page
+def showf2():
+    f1.pack_forget()
+    f2.pack()
 
-Button(win, text= "Log Workout", height= 5, width= 20, font= ('Helvetica 20 bold'), command= log).pack()
+# first page
+def showf1():
+    f2.pack_forget()
+    f1.pack()
+
+# adding an excercise function
+def add():
+    # take the string stored in clicked and apply the it to the label
+    def show(): 
+        label.config( text = clicked.get() )
+    # list of excercises
+    excercises = [
+        "Choose an Exercise", 
+        "Barbell Bench Press",
+        "Dumbell Bench Press",
+        "Tricep Pushdown",
+        "Lat Pulldown",
+        "Low Row",
+        "Dumbell Curls",
+        "Prescher Curls",
+        "Barbell Back Squat",
+        "Leg Extensions",
+        "Hamstring Curls",
+        "Hack Squat"
+    ]       
+    clicked = StringVar()
+    clicked.set("Choose an Exercise")  
+    drop = OptionMenu(f2 , clicked , *excercises,) 
+    drop.pack()  
+    button = Button( f2 , text = "Add" , command = show).pack()
+    label = Label( f2 , text = " ") 
+    label.pack()
+
+# on the first page, create a button to log a workout
+f1 = tk.Frame(win)
+log_button = tk.Button(f1, text="Log Workout", command=showf2)
+log_button.pack()
+f1.pack()
+
+# setup page two
+f2 = tk.Frame(win)
+
+add()
+
+
+
+
 
 
 win.mainloop()
-
