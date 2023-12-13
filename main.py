364e9575a315e7fd859c21312bdd7f98cb9c4846
Isaiah <isaiahmgarcia09@gmail.com>
2023-12-13 12:27:58 -0800

    working on saving function

--- main.py
+++ main.py
@@ -12,6 +12,8 @@
 
 from tkinter import *
 import tkinter as tk
+import pickle
+from setting import *
 
 win = tk.Tk()
 
@@ -27,54 +29,62 @@ def showf1():
 
 # adding an excercise function
 def add():
-    # list of excercises
-    excercises = [
-        "Choose an Exercise", 
-        "Barbell Bench Press",
-        "Dumbell Bench Press",
-        "Tricep Pushdown",
-        "Lat Pulldown",
-        "Low Row",
-        "Dumbell Curls",
-        "Prescher Curls",
-        "Barbell Back Squat",
-        "Leg Extensions",
-        "Hamstring Curls",
-        "Hack Squat"
-    ]       
+    # variable which can be changed but the set command
     clicked = StringVar()
+    # sets the intial string in the dropdown menu
     clicked.set("Select an Exercise")  
+    # expand the list in the dropdown menu
     drop = OptionMenu(f2 , clicked , *excercises) 
-    drop.grid(row=1, column=1)
+    # place the drop down menu
+    drop.grid(row=1, column=1, padx=5)
+    # create a list of all labels - theres none unitl the function runs
     labels = []
     # when add button is pressed, log the name of excercise, # of reps, etc
     def create_label():
+        # find number of labels
         count = len(f2.winfo_children())
+        # create a label with the string chosen from a list
         label = tk.Label(f2, text= " ")
+        # place the label
         label.grid(row=2 + 2*count, column=1)
+        # add the new label to the list of labels
         labels.append(label)
+        # place the label
         label.grid(row=2 + 2*count, column=1)
+        # set what the label text is
         label.config(text = clicked.get())
+        # shorten these labels because they will be used a lot
         reps_label = tk.Label(f2, text= "Reps")
         weight_label = tk.Label(f2, text= "Weight")
+        # create entry boxes to enter the # of reps and sets
         reps = tk.Entry(f2, width=5)
         weight = tk.Entry(f2, width=5)
+        # place the reps and sets labels and entry boxes
         reps_label.grid(row=2 + 2*count, column=2)
         weight_label.grid(row=2 + 2*count, column=3)
         reps.grid(row=3 + 2*count, column=2)
         weight.grid(row=3 + 2*count, column=3)
+    # create the add button, when clicked, run the function to create all the entry boxes, labels, etc
     log_button = tk.Button(f2, text="Add", command=create_label)
-    log_button.grid(row=1, column=2)
-    
+    # place the add button
+    log_button.grid(row=1, column=2, padx=5)
+
+def save():
+
+    pass
 
 # on the first page, create a button to log a workout
 f1 = tk.Frame(win)
-log_button = tk.Button(f1, text="Log Workout", command=showf2, font=('Helvetica bold', 20))
-log_button.pack()
+log_button = tk.Button(f1, text="Log Workout", command=showf2)
+log_button.grid()
 f1.pack()
 
 # setup page two
 f2 = tk.Frame(win)
 add()
+return_button= tk.Button(f2, text="Menu", command=showf1)
+return_button.grid(row=1, column=4, padx=5)
+done_button = tk.Button(f2, text="Done", command=save)
+done_button.grid(row= 1, column=3, padx=5)
 
 win.mainloop()
---
+++ setting.py
@@ -0,0 +1,20 @@
+# list of excercises
+excercises = [
+    "Choose an Exercise", 
+    "Barbell Bench Press",
+    "Dumbell Bench Press",
+    "Chest Flies",
+    "Tricep Pushdown",
+    "Skullcrushers",
+    "Lat Pulldown",
+    "Row Machine",
+    "Dumbell Rows",
+    "Barbell Curls",
+    "Dumbell Curls",
+    "Prescher Curls",
+    "Hammer Curls",
+    "Barbell Back Squat",
+    "Leg Extensions",
+    "Hamstring Curls",
+    "Hack Squat"
+]       
